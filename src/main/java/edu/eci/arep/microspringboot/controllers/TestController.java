bf1558d74ff6dd5c0bf2cbfa1e7e91db77baf209
Andres Marcelo <felipemarcelo156@gmail.com>
2020-08-28 19:03:25 -0500

    Final

--- src/main/java/edu/eci/arep/microspringboot/controllers/TestController.java
+++ src/main/java/edu/eci/arep/microspringboot/controllers/TestController.java
@@ -9,4 +9,24 @@ public class TestController {
         return "Greetings from Spring Boot!";
     }
 
+    @RequestMapping("/Pi")
+    public static String pi() {
+        return "Pi = "+  Math.PI;
+    }
+
+    @RequestMapping("/webapp")
+    public static String webapp() {
+        String line = "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<head>\n"
+                + "<meta charset=\"UTF-8\">\n"
+                + "<title>Title of the document</title>\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "<h1>Mi página usando anotaciones con Spring Boot :D</h1>\n"
+                + "</body>\n"
+                + "</html>\n";
+        return line;
+    }
+
 }
